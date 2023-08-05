1a0737bca2fe2bbabb73d6a0e89bda7b441d76ca
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2023-08-06 00:36:39 +0800

    Add files via upload

--- js-k3s2m3/resources.js
+++ js-k3s2m3/resources.js
@@ -135,6 +135,8 @@ var btnPlayTrainingVids = document.getElementsByClassName('btnPlayTrainingVids')
 function playTrainingVids() {
     var vidTitle = this.parentElement.children[0].innerText;
 
+    console.log(vidTitle);
+    
     black.style.opacity = 1;
     black.style.background = "rgba(0,0,0,0.3)";
     black.style.visibility = "visible";
@@ -225,7 +227,7 @@ function loadTrainingVideos() {
             for(var a = 0; a < btnPlayTrainingVids.length; a++) {
                 btnPlayTrainingVids[a].addEventListener('click', playTrainingVids);
             }
-            
+
         })
     }
     else {
