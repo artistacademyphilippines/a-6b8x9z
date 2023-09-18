f13494263a7a5b2ec8404c2cec8ab79c2a79ebe7
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2023-09-19 01:50:49 +0800

    Add files via upload

--- js-k3s2m3/view-files.js
+++ js-k3s2m3/view-files.js
@@ -513,7 +513,7 @@ function addNotifications() {
                                 if(childSnapshot.val().new) {
                                     //+2 to skip certi and training FRM
                                     //frm[newAppNo+1].children[2].children[newFileNo-1].children[0].children[0].style.visibility = "visible";
-                                    console.log(frm[newAppNo + 1].children[2].children[newFileNo-1]);
+                                    console.log(frm[newAppNo + 1].children[2]);
                                 }
                             })
                         })
